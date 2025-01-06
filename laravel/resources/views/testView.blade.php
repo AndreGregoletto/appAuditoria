e67fbea0d934be341030977606d6edf5999c2546
AndreGregoletto <argregoletto@gmail.com>
2025-01-06 11:34:47 -0300

    implementation/006 - Up view and controller test

--- laravel/resources/views/testView.blade.php
+++ laravel/resources/views/testView.blade.php
@@ -1,3 +1,42 @@
 <x-layout-default>
+    <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <input type="file" id="arquivo" name="arquivo">
+        <button type="submit">enviar</button>
+    </form>
 
+    <button onclick="destroy()">Destroy</button>
+    <script>
+        const baseUrl = window.location.origin;
+
+        function destroy() {
+            const inputFile = document.getElementById('arquivo');
+            if (inputFile.files.length === 0) {
+                alert("Por favor, selecione um arquivo.");
+                return;
+            }
+
+            const formData = new FormData();
+            formData.append('arquivo', inputFile.files[0]);
+
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                type: 'POST',
+                url: `${baseUrl}/test/`,
+                async: true,
+                data: formData,
+                contentType: false,
+                processData: false, 
+                success: function(data) {
+                    console.log("Arquivo enviado com sucesso:", data);
+                },
+                error: function(xhr, status, error) {
+                    console.error("Erro ao enviar o arquivo:", error);
+                }
+            });
+        }
+    
+    </script>
 </x-layout-default>
