0a5eb90afab59e9d409facd6305e5d59de7400a4
AndreGregoletto <argregoletto@gmail.com>
2025-02-22 13:10:06 -0300

    create containter user

--- laravel/resources/views/components/navbar.blade.php
+++ laravel/resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Noticias</a>
+                    <a class="nav-link" aria-current="page" href="#">Planos</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link {{ Route::currentRouteName() == 'navbarSendBalancete' ? 'active fst-italic' : '' }}" href="{{ route('navbarSendBalancete') }}">Enviar Balancete</a>
@@ -25,5 +25,21 @@
                 </li>
             </ul>
         </div>
+        <form class="d-flex" role="search">
+            <ul class="navbar-nav align-items-center">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Nome de Usuario</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Meu Plano</a></li>
+                        <li><a class="dropdown-item" href="#">Editar Perfil</a></li>
+                        <li><a class="dropdown-item disabled" href="#"><hr></a></li>
+                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <img src="https://picsum.photos/id/237/50/50" class="img-fluid rounded-5" alt="..." width="60" height="60">
+                </li>
+            </ul>
+        </form>
     </div>
 </nav>
